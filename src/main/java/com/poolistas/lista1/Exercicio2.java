6d772713835b14c7eba46376a8e4d98b8bf784d9
OBRETAS15 <juniorbretasaraujo305@gmail.com>
2024-09-03 15:45:56 -0300

    Exercicio 2

---
+++ src/main/java/com/poolistas/lista1/Exercicio2.java
@@ -0,0 +1,18 @@
+package com.poolistas.lista1;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void resolucao() {
+        String nome, sobrenome;
+
+        System.out.println("Digite seu nome: ");
+        Scanner scanner1 = new Scanner(System.in);
+        nome = scanner1.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+        sobrenome = scanner1.nextLine();
+
+        System.out.println("Olá, "+ nome +" "+sobrenome + " !");
+    }
+}
