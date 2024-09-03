89391ac655f3a54355483e191cabfe2823607efb
OBRETAS15 <juniorbretasaraujo305@gmail.com>
2024-09-03 17:08:13 -0300

    Exercicio 7

---
+++ src/main/java/com/poolistas/lista2/Exercicio7.java
@@ -0,0 +1,26 @@
+package com.poolistas.lista2;
+
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void resolucao2(){
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        double numero1 = scanner1.nextInt();
+
+        System.out.println("Digite outro número: ");
+        double numero2 = scanner1
+        if(numero1%numero2 == 0){
+            System.out.println("Número "+numero1+" é divisivel por "+numero2);
+        }
+        else if(numero2%numero1 == 0){
+            System.out.println("Número "+numero1+" é divisivel por "+numero2);
+        }
+        else{
+            System.out.println("Números informados não são divisiveis.");
+        }
+
+    scanner1.close();
+    }
+}    
--- src/main/java/com/poolistas/listasexercicios/ListasexerciciosApplication.java
+++ src/main/java/com/poolistas/listasexercicios/ListasexerciciosApplication.java
@@ -9,6 +9,7 @@ import com.poolistas.lista2.Exercicio3;
 import com.poolistas.lista2.Exercicio4;
 import com.poolistas.lista2.Exercicio5;
 import com.poolistas.lista2.Exercicio6;
+import com.poolistas.lista2.Exercicio7;
 
 @SpringBootApplication
 public class ListasexerciciosApplication {
@@ -16,7 +17,7 @@ public class ListasexerciciosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ListasexerciciosApplication.class, args);
 
-		Exercicio6.resolucao2();
+		Exercicio7.resolucao2();
 	}
 
 }
